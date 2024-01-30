790306ead8b3849dbee24719ab16c58a41529cec
SimonS2019 <shentiexiong@gmail.com>
2024-01-31 12:02:31 +1300

    82. Add home page layout and fetch recipes from API

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,10 +1,25 @@
+import { useFetch } from "../../hooks/useFetch";
+// import RecipeList from "../../components/RecipeList";
+
 // styles
-import './Home.css'
+import "./Home.css";
 
 export default function Home() {
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
   return (
-    <div>
-      Home
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {/* {data && <RecipeList recipes={data} />} */}
+      {data &&
+        data.map((recipe) => (
+          <div className="recipe-preview" key={recipe.id}>
+            <h2>{recipe.title}</h2>
+            <p>{recipe.cookingTime}</p>
+            <p>{recipe.servings}</p>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
