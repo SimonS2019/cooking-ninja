fa8eb4c9d48198eee23dcdd9fc36bc0cdc5d7509
SimonS2019 <shentiexiong@gmail.com>
2024-02-14 15:48:11 +1300

    jump to 101 success

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,25 +1,17 @@
-import { useFetch } from "../../hooks/useFetch";
-import RecipeList from "../../components/RecipeList";
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 
 // styles
-import "./Home.css";
+import './Home.css'
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
 
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
       {data && <RecipeList recipes={data} />}
-      {/* {data &&
-        data.map((recipe) => (
-          <div className="recipe-preview" key={recipe.id}>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.cookingTime}</p>
-            <p>{recipe.servings}</p>
-          </div>
-        ))} */}
     </div>
-  );
+  )
 }
